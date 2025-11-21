339fa5b459124e8bcc596d1dd58f79f0fd203ac5
jkusmierczyk <jakub.kusmierczyk@op.pl>
2025-11-21 21:38:48 +0100

    Change color

--- src/main/java/pob/pob_project/gui/ControlPanel.java
+++ src/main/java/pob/pob_project/gui/ControlPanel.java
@@ -40,7 +40,7 @@ public class ControlPanel extends VBox {
 
         setSpacing(5);
         setPadding(new Insets(5));
-        setStyle("-fx-background-color: #363535ff;");
+        setStyle("-fx-background-color: #252525ff;");
 
         Label title = new Label("Sterowanie symulacją");
         title.setStyle("-fx-text-fill: white; -fx-font-size: 14px;");
